685e003184cdda631e5e01d2a7a732e3dbfddd19
glassysky <873661157@qq.com>
2015-11-10 23:32:04 +0800

    app.js service.js

---
+++ learnAngular/myApp/public/javascripts/app.js
@@ -0,0 +1,17 @@
+angular.module('myApp', [
+	'ngRoute',
+	'myApp.services'
+])
+.controller('HomeController', function($scope, HitService){
+	HitService.count()
+		.then(function(data){
+			$scope.hits = data;
+		});
+
+	$scope.registerHit = function(){
+		HitService.registerHit()
+			.then(function(data){
+				$scope.hits = data;
+			});
+	}
+});
