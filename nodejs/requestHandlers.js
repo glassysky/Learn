5600ae61b251a1eeaad7f5298b35f4ebc9da9f8e
glassysky <873661157@qq.com>
2016-02-14 22:50:58 +0800

    chapter 04

--- nodejs/requestHandlers.js
+++ nodejs/requestHandlers.js
@@ -2,9 +2,10 @@
  * Created by cui on 2016/2/14.
  */
 var querystring = require("querystring"),
-    fs = require("fs");
+    fs = require("fs"),
+    formidable = require("formidable");
 
-function start(response, postData) {
+function start(response) {
     console.log("Request handler 'start' was called.");
 
     var body = '<html>'+
@@ -13,9 +14,10 @@ function start(response, postData) {
         'charset=UTF-8" />'+
         '</head>'+
         '<body>'+
-        '<form action="/upload" method="post">'+
-        '<textarea name="text" rows="20" cols="60"></textarea>'+
-        '<input type="submit" value="Submit text" />'+
+        '<form action="/upload" enctype="multipart/form-data" '+
+        'method="post">'+
+        '<input type="file" name="upload" multiple="multiple">'+
+        '<input type="submit" value="Upload file" />'+
         '</form>'+
         '</body>'+
         '</html>';
@@ -25,15 +27,22 @@ function start(response, postData) {
     response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
     console.log("Request handler 'upload' was called.");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("You've sent the text: " +
-        querystring.parse(postData).text);
-    response.end();
+
+    var form = new formidable.IncomingForm();
+    console.log("about to parse");
+    form.parse(request, function(error, fields, files) {
+        console.log("parsing done");
+        fs.renameSync(files.upload.path, "tmp/test.png");
+        response.writeHead(200, {"Content-Type": "text/html"})
+        response.write("received image:<br/>");
+        response.write("<img src='/show' />");
+        response.end();
+    });
 }
 
-function show(response, postData) {
+function show(response) {
     console.log("Request handler 'show' was called.");
     fs.readFile("tmp/test.png", "binary", function(error, file) {
         if(error) {
