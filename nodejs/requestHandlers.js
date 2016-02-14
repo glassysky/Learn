36ead9874f9436f2eca20ca9467148d925486905
glassysky <873661157@qq.com>
2016-02-14 22:20:00 +0800

    chapter 03

--- nodejs/requestHandlers.js
+++ nodejs/requestHandlers.js
@@ -1,7 +1,8 @@
 /**
  * Created by cui on 2016/2/14.
  */
-var querystring = require("querystring");
+var querystring = require("querystring"),
+    fs = require("fs");
 
 function start(response, postData) {
     console.log("Request handler 'start' was called.");
@@ -32,5 +33,21 @@ function upload(response, postData) {
     response.end();
 }
 
+function show(response, postData) {
+    console.log("Request handler 'show' was called.");
+    fs.readFile("tmp/test.png", "binary", function(error, file) {
+        if(error) {
+            response.writeHead(500, {"Content-Type": "text/plain"});
+            response.write(error + "/n");
+            response.end();
+        } else {
+            response.writeHead(200, {"Content-Type": "image/png"});
+            response.write(file, "binary");
+            response.end();
+        }
+    });
+}
+
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
