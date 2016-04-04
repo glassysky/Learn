eaa73054cb0831803df70ffa5656cf1d74ccf295
glassysky <873661157@qq.com>
2016-04-04 23:14:53 +0800

    set charset

--- scrapy/douyu/douyu/pipelines.py
+++ scrapy/douyu/douyu/pipelines.py
@@ -6,6 +6,23 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class DouyuPipeline(object):
+# class DouyuPipeline(object):
+#     def process_item(self, item, spider):
+#         return item
+
+
+import json
+import codecs
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+class JsonWritePipeline(object):
+    def __init__(self):
+        self.file = codecs.open('myItem.json', mode='wb', encoding='utf-8')
+
     def process_item(self, item, spider):
+        line = "[" + json.dumps(dict(item), ensure_ascii=False) + "]\n"
+        self.file.write(line)
         return item
