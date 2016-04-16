329835e8d10a3b627074e0e760b660c8c119d36d
glassysky <873661157@qq.com>
2016-04-16 23:05:43 +0800

    webpack init

---
+++ react/webpack.config.js
@@ -0,0 +1,18 @@
+/**
+ * Created by cuitianhao on 16/4/16.
+ */
+module.exports = {
+    entry: './entry.js',
+    output: {
+        path: __dirname,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: 'style!css'
+            }
+        ]
+    }
+}
