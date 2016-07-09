fbfd4975d663afd97ded3570b2d39dac46be50d4
glassysky <873661157@qq.com>
2016-07-09 17:25:19 +0800

    os program test

---
+++ OS/BOOL Party/BOOL Party/BOOL Party/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  BOOL Party
+//
+//  Created by 崔天昊 on 7/9/16.
+//  Copyright © 2016 崔天昊. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Tire : NSObject
+@end
+
+@implementation Tire
+
+- (NSString *) description
+{
+    return (@"I am a tire. I last a while");
+} //description
+
+@end //Tire
