6923a83c662a197fcdf82d97451909cc180b0b2f
glassysky <873661157@qq.com>
2016-10-19 00:18:07 +0800

    algorithm project init

---
+++ algorithm/Algorithm/Algorithm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Algorithm
+//
+//  Created by 崔天昊 on 10/19/16.
+//  Copyright © 2016 Cui Tianhao. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
