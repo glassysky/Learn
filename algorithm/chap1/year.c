2e806b92a60d8b78c57805b9216fdce1e98e62f9
glassysky <873661157@qq.com>
2016-10-19 22:58:41 +0800

    year

---
+++ algorithm/chap1/year.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    int year;
+    scanf("%d", &year);
+    if (year%400 == 0 || (year%4 == 0 && year%100 != 0)) {
+        printf("yes\n");
+    } else {
+        printf("no\n");
+    }
+    return 0;
+}
+    
