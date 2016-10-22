7e5eb362a5c1ef567cbce59eb22a702a3f48f725
glassysky <873661157@qq.com>
2016-10-22 23:09:34 +0800

    daffodil

---
+++ algorithm/chap2/daffodil.c
@@ -0,0 +1,22 @@
+/*************************************************************************
+	> File Name: daffodil.c
+	> Author: 
+	> Mail: 
+	> Created Time: Sat Oct 22 22:56:24 2016
+ ************************************************************************/
+
+#include<stdio.h>
+#include<math.h>
+int main() {
+    int ones, tens, hundreds;
+    for (hundreds = 1; hundreds < 10; hundreds++) {
+        for (tens = 0; tens < 10; tens++) {
+            for (ones = 0; ones < 10; ones++) {
+                if ((hundreds*100 + tens*10 + ones) == (pow(hundreds, 3) + pow(tens, 3) + pow(ones, 3))) {
+                    printf("%d\n", hundreds*100 + tens*10 + ones);
+                }
+            }
+        }
+    }
+    return 0;
+}
