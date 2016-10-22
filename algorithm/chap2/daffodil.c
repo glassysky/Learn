7b22e0b66d68fdeace83ee73f3db3b081601f539
glassysky <873661157@qq.com>
2016-10-22 23:13:13 +0800

    daffodil output to a file

--- algorithm/chap2/daffodil.c
+++ algorithm/chap2/daffodil.c
@@ -8,15 +8,18 @@
 #include<stdio.h>
 #include<math.h>
 int main() {
+    FILE * fout;
+    fout = fopen("daffodil.out", "wb");
     int ones, tens, hundreds;
     for (hundreds = 1; hundreds < 10; hundreds++) {
         for (tens = 0; tens < 10; tens++) {
             for (ones = 0; ones < 10; ones++) {
                 if ((hundreds*100 + tens*10 + ones) == (pow(hundreds, 3) + pow(tens, 3) + pow(ones, 3))) {
-                    printf("%d\n", hundreds*100 + tens*10 + ones);
+                    fprintf(fout, "%d\n", hundreds*100 + tens*10 + ones);
                 }
             }
         }
     }
+    fclose(fout);
     return 0;
 }
