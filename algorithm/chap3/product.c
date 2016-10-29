fc5099e87062b9aec2baee8a40cdc12acd6109dd
glassysky <873661157@qq.com>
2016-10-29 11:47:00 +0800

    product.c

---
+++ algorithm/chap3/product.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: product.c
+	> Author: 
+	> Mail: 
+	> Created Time: Tue Oct 25 20:10:21 2016
+ ************************************************************************/
+
+#include<stdio.h>
+int main() {
+    char a;
+    while((a = getchar()) != EOF) {
+        printf("%c", a);
+    }
+    return 0;
+}
