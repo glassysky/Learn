4aa0d9689382668be5a9c1f4bacffa8ffb0900c9
glassysky <873661157@qq.com>
2016-11-16 12:30:13 +0800

    temperatrue.py

--- py/test.py
+++ py/test.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+val = input("please input temperature with signal(e.g. 32C)\n")
+if val[-1] in ['C', 'c']:
+    f = 1.8 * float(val[0:-1]) + 32
+    print("value after transfered: %.2fF"%f)
+elif val[-1] in ['F', 'f']:
+    c = (float(val[0:-1]) - 32) / 1.8
+    print("value after transfered: %.2fC"%c)
+else:
+    print("input error")
