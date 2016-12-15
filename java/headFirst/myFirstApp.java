43d563e70e3e569fb769c859032418460a6eae69
glassysky <873661157@qq.com>
2016-12-16 00:03:00 +0800

    java first

---
+++ java/headFirst/myFirstApp.java
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: myFirstApp.java
+	> Author: 
+	> Mail: 
+	> Created Time: Fri Dec 16 00:00:41 2016
+ ************************************************************************/
+
+public class myFirstApp
+{
+    public static void main (String[] args) {
+        System.out.println("I Rule");
+        System.out.println("The World");
+    }
+}
