5b104d29fcaf357a36b2b8a0fc0c0b598cde7fd1
glassysky <873661157@qq.com>
2016-12-17 22:21:38 +0800

    java string test

---
+++ java/headFirst/PhraseCMatic.java
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: PhraseCMatic.java
+	> Author: 
+	> Mail: 
+	> Created Time: Fri Dec 16 00:14:02 2016
+ ************************************************************************/
+
+public class PhraseCMatic
+{
+    public static void main (String[] args) {
+        String[] wordListOne = {
+            "24/7", "multiTier", "30,000 foot", "B-to-B", "win-win", "frontend", "web-based", "pervasive", "smart"
+        };
+        String phrase = wordListOne[0] + " " + wordListOne[1] + " " +  wordListOne[2];
+        System.out.println("test my wtf " + phrase);
+    }
+}
